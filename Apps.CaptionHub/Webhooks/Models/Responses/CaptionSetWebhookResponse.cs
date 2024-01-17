c9e43f0ee6e2f43e87fba2162c448c0fa16648c2
bZverok <137277669+bZverok@users.noreply.github.com>
2024-01-17 17:43:51 +0200

    Minor fixes (#13)
    * wait until for caption set
    
    * Fixed description of wait for
    
    * project actions minor improvements
    
    * project actions fixes
    
    * Percentage complete reference in await
    
    * improved caption set percentage type
    
    * added translation and transcription providers
    
    * added custom dictionary support
    
    * increased version
    
    * commented redundant action
    
    * increased version
    
    * added handlers and models for webhooks
    
    * added webhooks
    
    * fixed recursive payload
    
    * CI: update workflow to invoke DEV deployment
    
    * minor webhook fixes
    
    * increased version
    
    ---------
    
    Co-authored-by: msonnemans <mathijssonnemans@outlook.com>
    Co-authored-by: mathijs-bb <mathijs.sonnemans@blackbird.io>
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>

--- Apps.CaptionHub/Webhooks/Models/Responses/CaptionSetWebhookResponse.cs
+++ Apps.CaptionHub/Webhooks/Models/Responses/CaptionSetWebhookResponse.cs
@@ -1,8 +1,10 @@
 using Apps.CaptionHub.Webhooks.Models.Payloads.CaptionSet;
+using Blackbird.Applications.Sdk.Common;
 
 namespace Apps.CaptionHub.Webhooks.Models.Responses;
 
 public class CaptionSetWebhookResponse : ProjectWebhookResponse
 {
+    [Display("Caption set")]
     public CaptionSetPayload CaptionSet { get; set; }
 }
