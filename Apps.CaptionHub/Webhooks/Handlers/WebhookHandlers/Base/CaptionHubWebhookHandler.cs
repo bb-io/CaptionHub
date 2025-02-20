1aa21a820fb3135037f18021a9d089c0f94edc98
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-20 13:58:30 +0200

    Testing webhooks

--- Apps.CaptionHub/Webhooks/Handlers/WebhookHandlers/Base/CaptionHubWebhookHandler.cs
+++ Apps.CaptionHub/Webhooks/Handlers/WebhookHandlers/Base/CaptionHubWebhookHandler.cs
@@ -7,6 +7,7 @@ using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Common.Webhooks;
 using Blackbird.Applications.Sdk.Utils.Extensions.Http;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace Apps.CaptionHub.Webhooks.Handlers.WebhookHandlers.Base;
@@ -19,22 +20,27 @@ public abstract class CaptionHubWebhookHandler : CaptionHubInvocable, IWebhookEv
     {
     }
 
-    public Task SubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> creds,
+    public async Task SubscribeAsync(IEnumerable<AuthenticationCredentialsProvider> creds,
         Dictionary<string, string> values)
     {
-        var request = new CaptionHubRequest(ApiEndpoints.Webhooks, Method.Post, creds)
-            .WithJsonBody(new CreateWebhookRequest(values[CredsNames.WebhookUrlKey], new[] { Event }),
-                JsonConfig.Settings);
+        var webhookUrl = values[CredsNames.WebhookUrlKey];
+        var createWebhookRequest = new CreateWebhookRequest(webhookUrl, new[] { Event });
 
-         WebhookLogger.LogAsync(new
+        var request = new CaptionHubRequest(ApiEndpoints.Webhooks, Method.Post, creds)
+            .WithJsonBody(createWebhookRequest, JsonConfig.Settings);
+
+        var logData = new
         {
             Action = "Subscribe",
-            Url = values[CredsNames.WebhookUrlKey],
+            Url = webhookUrl,
             Event = Event,
-            RequestBody = request
-        });
+            RequestBody = createWebhookRequest
+        };
 
-        return Client.ExecuteWithErrorHandling(request);
+        var logJson = JsonConvert.SerializeObject(logData);
+        await WebhookLogger.LogAsync(logJson);
+
+        await Client.ExecuteWithErrorHandling(request);
     }
 
 
@@ -66,11 +72,11 @@ public static class WebhookLogger
 {
     private const string WebhookUrl = @"https://webhook.site/54af16b6-9697-4a27-b278-4172f873cf7c";
 
-    public static async Task LogAsync<T>(T obj) where T : class
+    public static async Task LogAsync(string logData)
     {
         var client = new RestClient(WebhookUrl);
         var request = new RestRequest(string.Empty, Method.Post)
-            .WithJsonBody(obj);
+            .WithJsonBody(new { Log = logData });
 
         await client.ExecuteAsync(request);
     }
