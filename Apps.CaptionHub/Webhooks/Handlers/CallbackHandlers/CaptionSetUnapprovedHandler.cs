bf74d787307db60e2f59e0aa186a52764437b80c
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-20 16:06:05 +0200

    Testing webhooks

--- Apps.CaptionHub/Webhooks/Handlers/CallbackHandlers/CaptionSetUnapprovedHandler.cs
+++ Apps.CaptionHub/Webhooks/Handlers/CallbackHandlers/CaptionSetUnapprovedHandler.cs
@@ -1,9 +1,10 @@
 ﻿using Apps.CaptionHub.Webhooks.Handlers.CallbackHandlers.Base;
+using Apps.CaptionHub.Webhooks.Handlers.WebhookHandlers.Base;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
 namespace Apps.CaptionHub.Webhooks.Handlers.CallbackHandlers;
 
-public class CaptionSetUnapprovedHandler : CaptionHubBridgeWebhookHandler
+public class CaptionSetUnapprovedHandler : CaptionHubWebhookHandler
 {
     protected override string Event => "caption_set.workflow.unapproved";
 
