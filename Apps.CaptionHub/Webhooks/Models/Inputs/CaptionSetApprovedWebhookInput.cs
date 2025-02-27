104abb444345ef3959bfcb7e5141688eec27fdf6
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-27 17:10:27 +0200

    Added filter to on action set approved

---
+++ Apps.CaptionHub/Webhooks/Models/Inputs/CaptionSetApprovedWebhookInput.cs
@@ -0,0 +1,16 @@
+﻿using Apps.CaptionHub.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.CaptionHub.Webhooks.Models.Inputs
+{
+    public class CaptionSetApprovedWebhookInput : ProjectWebhookInput
+    {
+        [Display("Caption set ID")]
+        public string? CaptionSetId { get; set; }
+
+        [Display("Language ID")]
+        [DataSource(typeof(LanguageIdDataHandler))]
+        public IEnumerable<string>? LanguageIds { get; set; }
+    }
+}
