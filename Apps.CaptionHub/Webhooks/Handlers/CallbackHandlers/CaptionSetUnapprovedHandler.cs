2851d2d40bb0ca834a5ab621fadd9b54c5f95619
RiabushenkoA <ariabushenko@blackbird.io>
2025-03-14 20:12:21 +0200

    Updated event on caption set

--- Apps.CaptionHub/Webhooks/Handlers/CallbackHandlers/CaptionSetUnapprovedHandler.cs
+++ Apps.CaptionHub/Webhooks/Handlers/CallbackHandlers/CaptionSetUnapprovedHandler.cs
@@ -1,14 +1,17 @@
-﻿using Apps.CaptionHub.Webhooks.Handlers.CallbackHandlers.Base;
+﻿using Apps.CaptionHub.Webhooks.Bridge.Base;
+using Apps.CaptionHub.Webhooks.Handlers.CallbackHandlers.Base;
 using Apps.CaptionHub.Webhooks.Handlers.WebhookHandlers.Base;
+using Apps.CaptionHub.Webhooks.Models.Inputs;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Webhooks;
 
 namespace Apps.CaptionHub.Webhooks.Handlers.CallbackHandlers;
 
-public class CaptionSetUnapprovedHandler : CaptionHubWebhookHandler
+public class CaptionSetUnapprovedHandler : BaseWebhookBridgeHandler
 {
-    protected override string Event => "caption_set.workflow.unapproved";
+    public static  string Event => "caption_set.workflow.unapproved";
 
-    public CaptionSetUnapprovedHandler(InvocationContext invocationContext) : base(invocationContext)
+    public CaptionSetUnapprovedHandler(InvocationContext invocationContext, [WebhookParameter] CaptionSetApprovedWebhookInput input) : base(invocationContext, Event, input.ProjectId)
     {
     }
 }
